73c1aec00caea99c339be4ae1cc1af19849c2874
Saurav Sahu <sauravsahu.12@live.com>
2020-02-10 11:42:09 +0530

    improv: prop types for todo-list

--- src/components/todo-list/index.js
+++ src/components/todo-list/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PT from 'prop-types';
 import { Card } from '../card';
 
 export const TodoList = ({ todos, onTodoComplete }) => (
@@ -15,3 +16,16 @@ export const TodoList = ({ todos, onTodoComplete }) => (
         }
   </div>
 );
+
+TodoList.propTypes = {
+  todos: PT.arrayOf({
+    id: PT.string.isRequired,
+    title: PT.string.isRequired,
+    done: PT.bool.isRequired,
+  }),
+  onTodoComplete: PT.func.isRequired,
+};
+
+TodoList.defaultProps = {
+  todos: [],
+};
