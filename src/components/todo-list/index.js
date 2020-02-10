4519536af4c4ef385d9192e23ba48829e1f16dfe
Saurav Sahu <sauravsahu.12@live.com>
2020-02-10 10:59:30 +0530

    component: todo list

---
+++ src/components/todo-list/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Card } from '../card'
+ 
+export const TodoList = ({ todos, onTodoComplete}) => (
+    <div>
+        <div>All To-dos</div>
+        {
+            todos.map((todo) => (
+                <Card
+                    key={todo.id}
+                    title={todo.title}
+                    onDone={() => onTodoComplete(todo.id)}
+                />
+))
+        }
+    </div>
+)
---
+++ src/components/todo-list/stories.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+import { TodoList } from ".";
+
+
+export default {
+    title: 'TodoList',
+    component: TodoList
+}
+
+const todos = [
+    {
+        id: 1,
+        title: 'Finish the todo app',
+    },
+    {
+        id: 2,
+        title: 'Review todo app code',
+    },
+    {
+        id: 3,
+        title: 'Cover React life cycle events',
+    }
+]
+
+export const Default = () => (
+    <TodoList
+        todos={todos}
+        onTodoComplete={action('onTodoComplete')}
+    />
+)
